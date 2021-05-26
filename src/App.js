0bfe0ff219b90490a32874aa72c7364d66805704
Baboo523 <babooheerani999@gmail.com>
2021-05-27 00:17:15 +0500

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-
-function App() {
+function App() 
+{
   return (
     <div className="App">
       <h1>Hello World</h1>
     </div>
   );
 }
-
 export default App;
